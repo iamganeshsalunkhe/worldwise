f8d893e3a7abc1b9724bd35aba936bc2dbd51779
Ganesh  Salunkhe <ganeshsalunkhe1998@gmail.com>
2024-06-06 11:27:52 +0530

    Sidebar Components added partially.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import {BrowserRouter,Routes, Route} from 'react-router-dom'
+import { useEffect, useState } from 'react';
+import {BrowserRouter,Routes, Route, Navigate} from 'react-router-dom'
+
 import Product from './pages/product';
 import Homepage from './pages/Homepage';
 import Pricing from './pages/Pricing';
@@ -6,7 +8,9 @@ import PageNotFound from './pages/PageNotFound';
 import Applayout from './pages/Applayout';
 import Login from './pages/Login';
 import CityList from './Components/CityList';
-import { useEffect, useState } from 'react';
+import CountryList from './Components/CountryList';
+import City from './Components/City';
+import Form from './Components/Form';
 
 const URL =`http://localhost:7000`
 
@@ -37,13 +41,15 @@ function App() {
         <Route path='pricing' element ={<Pricing/>}/>
         <Route path='login' element={<Login/>}/>
         <Route path = 'app' element={<Applayout/>}> 
-          <Route index element={<CityList cities= {cities} isLoading={isLoading }/>}
+          <Route index element={<Navigate replace to='cities'/>}
           />
           <Route path='cities' element ={<CityList cities ={cities} isLoading ={isLoading}/>}
           />
-          <Route path='countries' element={<p>Countries</p>}/>
+          <Route path='cities/:id' element={<City/>}/>
+          <Route path='countries' element={<CountryList cities={cities} isLoading={isLoading}/> }
+          />
+          <Route path='form' element={<Form />}/>
           </Route>
-          <Route path='form' element={<p>Form</p>}/>
         <Route path='*' element= {<PageNotFound/>}/>
       </Routes>
     </BrowserRouter>
--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -1,8 +1,21 @@
+/* eslint-disable no-unused-vars */
+import { useNavigate, useSearchParams } from 'react-router-dom'
 import styles from './Map.module.css'
 function Map() {
+    const navigate = useNavigate()
+    const [serachParams, setSerachParams]=useSearchParams();
+    const lat = serachParams.get('lat')
+    const lng = serachParams.get('lng')
+
     return (
-        <div className={styles.mapContainer }>
-            MAP            
+        <div className={styles.mapContainer } onClick={() =>{
+            navigate("form")
+        }}>
+            <h1>MAP</h1>
+            <h1>Position : {lat} {lng}</h1>  
+            <button onClick={() =>{
+                setSerachParams({lat:24,lng:11})
+            }}>Change pos</button>          
         </div>
     )
 }
